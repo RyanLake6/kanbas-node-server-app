b7c695fd1ba3c194ece7e4192f4fc6c7e5bfc416
Ryan Lake <73082037+RyanLake6@users.noreply.github.com>
2024-03-31 20:44:36 -0400

    finished 4

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import express from "express";
 import cors from "cors";
 const app = express();
 app.use(cors());
-app.use(express.json());
+CourseRoutes(app);
+ModuleRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(4000);
